2d83e16460f00e5e07078435a51b521c73ba4a3a
John C. Lavelle <jlavelle604@gmail.com>
2019-11-28 12:21:00 -0500

    2015 Day 8 p2

--- 2015/src/day8.rkt
+++ 2015/src/day8.rkt
@@ -2,11 +2,15 @@
 
 (define (read-input) (file->lines "../inputs/day8"))
 
-(define (mem-vs-code-len strs)
-  (define code-sum (for/sum ([s strs]) (string-length s)))
-  (define mem-sum
-    (for/sum ([s (map (λ (s) (read (open-input-string s))) strs)])
-      (if (equal? eof s) 0 (string-length s))))
-  (values mem-sum code-sum))
+(define (code-sum strs) (for/sum ([s strs]) (string-length s)))
 
-(define (solve1 strs) (let-values ([(m c) (mem-vs-code-len strs)]) (- c m)))
+(define (mem-sum strs)
+  (for/sum ([s (map (λ (s) (read (open-input-string s))) strs)])
+    (if (equal? eof s) 0 (string-length s))))
+
+(define (enc-sum strs)
+  (for/sum ([s strs])
+    (string-length (~v s))))
+
+(define (solve1 strs) (- (code-sum strs) (mem-sum strs)))
+(define (solve2 strs) (- (enc-sum strs) (code-sum strs)))
