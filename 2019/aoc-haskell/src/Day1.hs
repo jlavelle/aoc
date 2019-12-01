cccc52be9ee1b4203282358db4b9ca5e27485a17
John C. Lavelle <jlavelle604@gmail.com>
2019-12-01 11:54:52 -0500

    2019 Day 1

---
+++ 2019/aoc-haskell/src/Day1.hs
@@ -0,0 +1,37 @@
+ module Day1 where
+{-# LANGUAGE BlockArguments #-}
+
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import qualified Data.Text.Read as T
+import Data.Either (fromRight)
+import Data.Monoid (Sum(..))
+import Data.Foldable (traverse_, fold)
+import Data.List (unfoldr)
+
+fuelReq :: Integral a => a -> a
+fuelReq n = div n 3 - 2
+
+fuelFuelReq :: Integral a => a -> a
+fuelFuelReq = foldr (+) 0 . unfoldr go . fuelReq
+  where
+    go x | x <= 0    = Nothing
+         | otherwise = Just (x, fuelReq x)
+
+parseInput :: IO (Either String [Int])
+parseInput = traverse (fmap fst . T.decimal) . T.lines <$> T.readFile "inputs/day1"
+
+solve1 :: (Foldable f, Integral a) => f a -> Sum a
+solve1 = foldMap (Sum . fuelReq)
+
+solve2 :: (Foldable f, Integral a) => f a -> Sum a
+solve2 = foldMap \a -> 
+  let mreq = fuelReq a
+  in Sum mreq <> Sum (fuelFuelReq mreq)
+
+solutions :: IO ()
+solutions = do
+  ei <- parseInput
+  traverse_ (print . solve1) ei
+  traverse_ (print . solve2) ei
