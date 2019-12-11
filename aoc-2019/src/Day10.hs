d5a4b6eaf35439d25c26d6bdf7bdf023b8e8ccff
John C. Lavelle <jlavelle604@gmail.com>
2019-12-10 20:55:59 -0500

    2019 Day 10 p2

--- aoc-2019/src/Day10.hs
+++ aoc-2019/src/Day10.hs
@@ -4,14 +4,25 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Set (Set)
 import qualified Data.Set as Set
-import Data.Functor ((<&>))
+import Data.Functor ((<&>), ($>))
+import Lens.Micro ((^?), ix)
 import Data.Bifunctor (bimap)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import Data.Ord (comparing)
+import Data.Foldable (maximumBy, fold)
+import Data.List (sortBy, transpose)
 
 type Point a = (a, a)
 
-data Ray a = Ray a (Point a) deriving (Eq, Ord, Show)
+data Ray a = Ray a (Point a) 
+  deriving (Eq, Ord, Show)
+
+angle :: Ray a -> a
+angle (Ray a _) = a
+
+origin :: Ray a -> Point a
+origin (Ray _ a) = a
 
 convert :: (Integral a, Num b) => Point a -> Point b
 convert = bimap fromIntegral fromIntegral
@@ -20,23 +31,21 @@ rayThrough :: (Eq a, RealFloat a) => Point a -> Point a -> Ray a
 rayThrough p1@(x1, y1) (x2, y2) =
   let dx = x2 - x1
       dy = y2 - y1
-  in Ray (atan2 dy dx) p1
+  in Ray (atan2 (-dx) dy) p1
 
-visibilities :: Set (Point Int) -> Map (Point Int) Int
+visibilities :: Set (Point Int) -> Map (Point Int) (Map (Ray Double) [Point Int])
 visibilities field = foldr go mempty field
   where
-    go a m =
-      let ls = foldr lines (mempty :: Set (Ray Double)) field
-      in Map.insert a (length ls) m
+    go a m = Map.insert a (foldr lines mempty $ Set.delete a field) m
       where
-        lines b = Set.insert (rayThrough (convert a) (convert b))
+        lines b = Map.insertWith (<>) (rayThrough (convert a) (convert b)) [b]
 
 toField :: [[Bool]] -> Set (Point Int)
 toField = foldMap go . zip [0..]
   where
-    go (y, l) = Set.fromList 
-              $ fmap fst 
-              $ filter snd 
+    go (y, l) = Set.fromList
+              $ fmap fst
+              $ filter snd
               $ zipWith (\x b -> ((x, y), b)) [0..] l
 
 parseField :: T.Text -> Set (Point Int)
@@ -45,10 +54,22 @@ parseField = toField . fmap (T.foldr (\c a -> (c == '#') : a) []) . T.lines
 parseInput :: IO (Set (Point Int))
 parseInput = parseField <$> T.readFile "inputs/day10"
 
+stationLocation :: Set (Point Int) -> Map (Ray Double) [Point Int]
+stationLocation = maximumBy (comparing length) . visibilities
+
+solve1 :: Set (Point Int) -> Int
+solve1 = length . stationLocation
+
+solve2 :: Set (Point Int) -> Maybe (Point Int)
+solve2 = (^? ix 199) . fold . transpose . fmap (sortBy (comparing pabs) . snd) . Map.toAscList . stationLocation
+  where
+    pabs (a, b) = (abs a, abs b)
+
 solutions :: IO ()
 solutions = do
   i <- parseInput
-  print $ maximum $ visibilities i
+  print $ solve1 i
+  print $ solve2 i
 
 test1 :: Set (Point Int)
 test1 = parseField ".#..#..###\n####.###.#\n....###.#.\n..###.##.#\n##.##.#.#.\n....###..#\n..#.#..#.#\n#..#.#.###\n.##...##.#\n.....#.#.."
